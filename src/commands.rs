375bc2d88f4311abacfc8cf25b0184205419f123
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2024-02-04 12:38:31 -0600

    Separate command logic from TUI main loop

---
+++ src/commands.rs
@@ -0,0 +1,54 @@
+use anyhow::{Result, Context};
+use crossterm::event::KeyCode;
+
+use crate::AppState;
+
+
+pub enum AppCommand {
+    Quit,
+    Increment,
+    Decrement
+}
+
+impl AppCommand {
+    /// Returns a command if the given KeyCode corresponds to one
+    pub fn from_key(key: KeyCode) -> Option<Self> {
+        match key {
+            KeyCode::Char('j') => Some(Self::Increment),
+            KeyCode::Char('k') => Some(Self::Decrement),
+            KeyCode::Char('q') => Some(Self::Quit),
+            _ =>  None
+        }
+    }
+
+    /// Apply this command to the app state
+    pub fn run(&self, app_state: &mut AppState) -> Result<()> {
+        match self {
+            AppCommand::Quit => {
+                AppCommand::quit(app_state);
+                Ok(())
+            },
+            AppCommand::Increment => {
+                AppCommand::increment_counter(app_state);
+                Ok(())
+            },
+            AppCommand::Decrement => {
+                AppCommand::decrement_counter(app_state);
+                Ok(())
+            },
+        }
+    }
+
+    fn quit(app_state: &mut AppState) {
+        app_state.should_quit = true;
+    }
+
+    fn increment_counter(app_state: &mut AppState) {
+        app_state.counter += 1;
+    }
+
+    fn decrement_counter(app_state: &mut AppState) {
+        app_state.counter -= 1;
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,21 @@
 #![allow(unused_imports)]
 
 use anyhow::{Result, Context};
+use commands::AppCommand;
 use crossterm::event::{
     Event,
     KeyEventKind, KeyCode,
 };
 use ratatui::{prelude::CrosstermBackend, Terminal, Frame, widgets::Paragraph};
 
+mod commands;
+
 pub struct AppState {
     counter: i32,
     should_quit: bool,
 }
 
-
 fn main() -> Result<()> {
-
-
     // Enable raw mode
     crossterm::terminal::enable_raw_mode()
         .context("Failed to enable raw mode.")?;
@@ -28,97 +28,60 @@ fn main() -> Result<()> {
         .context("Failed to start new Terminal with CrosstermBackend.")?;
 
     // Define application state
-    let mut app = AppState {
+    let mut app_state = AppState {
         counter: 0,
         should_quit: false,
     };
 
-
-    loop {
+    while !app_state.should_quit {
         terminal.draw(|frame: &mut Frame| {
-            frame.render_widget(Paragraph::new(format!("Counter: {}", app.counter)), frame.size());
-            update().map_
-            match update() {
-
-            }
+            frame.render_widget(Paragraph::new(format!("Counter: {}", app_state.counter)), frame.size());
         })?;
 
-        // Poll for keyboard events
+        let key_opt = poll_for_keypress();
 
+        let command_opt: Option<AppCommand> = if let Result::Ok(key_event) = key_opt {
+            match key_event {
+                Some(keycode) => AppCommand::from_key(keycode),
+                None => None
+            }
+        } else { None };
+
+        // If command is present, run it
+        if let Some(command) = command_opt {
+            // TODO - map over command_opt
+            let updated = command.run(&mut app_state);
+            match updated {
+                Ok(_) => continue, // If all is good, move on to the next frame.
+                Err(_) => break,   // If we couldn't apply the command, stop due to error.
+            };
+        };
     }
 
-    // Broke out of loop, initiate shutdown
+    // Broke out of loop, initiate shutdown.
     crossterm::execute!(std::io::stderr(), crossterm::terminal::LeaveAlternateScreen)?;
     crossterm::terminal::disable_raw_mode()?;
 
     Ok(())
 }
 
-pub enum AppCommand {
-    Quit,
-    Increment,
-    Decrement
-}
 
-impl AppCommand {
-    pub fn from_key(key: KeyCode) -> Option<Self> {
-        match key {
-            KeyCode::Char('j') => Some(Self::Increment),
-            KeyCode::Char('k') => Some(Self::Decrement),
-            KeyCode::Char('q') => Some(Self::Quit),
-            _ =>  None
-        }
-    }
-}
-
-fn update(app_state: &mut AppState) -> Result<()> {
-
-    fn poll_for_keypress() -> Result<Option<KeyCode>> {
-        if let Event::Key(key) = crossterm::event::read().context("Could not read event.")? {
-            // TODO - Add handling for other kinds of events
-            if key.kind == KeyEventKind::Press {
-                match key.code {
-                    KeyCode::Char('j') => Ok(Some(KeyCode::Char('j'))),
-                    KeyCode::Char('k') => Ok(Some(KeyCode::Char('k'))),
-                    KeyCode::Char('q') => Ok(Some(KeyCode::Char('q'))),
-                    _ => Ok(None),
-                }
-            } else {
-                Ok(None)
+fn poll_for_keypress() -> Result<Option<KeyCode>> {
+    if let Event::Key(key) = crossterm::event::read().context("Could not read event.")? {
+        // TODO - Add handling for other kinds of events
+        if key.kind == KeyEventKind::Press {
+            match key.code {
+                KeyCode::Char('j') => Ok(Some(KeyCode::Char('j'))),
+                KeyCode::Char('k') => Ok(Some(KeyCode::Char('k'))),
+                KeyCode::Char('q') => Ok(Some(KeyCode::Char('q'))),
+                _ => Ok(None),
             }
         } else {
             Ok(None)
         }
+    } else {
+        Ok(None)
     }
-
-    let key_event_opt = poll_for_keypress().context("Could not poll for keypress.");
-
-    let command_opt = if let Result::Ok(key_event) = key_event_opt {
-        match key_event {
-            Some(keycode) => Some(AppCommand::from_key(keycode)),
-            None => None
-        }
-    } else { None };
-
-    // If command is present, run it
-    if let Some(command) = command_opt {
-        // TODO - map over command_opt
-
-        match command {
-            AppCommand::Quit => {
-                // FIXME
-                Ok(())
-            },
-            AppCommand::Increment => {
-                app_state.counter += 1;
-                Ok(())
-            },
-            AppCommand::Decrement => {
-                app_state.counter -=1;
-                Ok(())
-            }
-        }
-    } else { Ok(()) }
 }
 
 fn wait() -> Result<()> {
@@ -127,5 +90,4 @@ fn wait() -> Result<()> {
 
     Ok(())
 }
-
 
