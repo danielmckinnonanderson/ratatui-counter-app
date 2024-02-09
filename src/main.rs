bdcfa0030a78d1ba84d18ee29bab3cc56c29e6a3
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2024-02-09 13:45:33 -0600

    Limit frame rate

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
+use std::time::Instant;
+
 use anyhow::{Context, Result};
 use commands::AppCommand;
-use crossterm::{event::{Event, KeyCode, KeyEventKind}, style::Colored};
+use crossterm::{
+    event::{Event, KeyCode, KeyEventKind},
+    style::Colored, terminal::enable_raw_mode,
+};
 use lazy_static::lazy_static;
-use ratatui::{prelude::{CrosstermBackend, Rect, Layout, Direction, Constraint}, widgets::{Paragraph, Block, Borders}, Frame, Terminal, text::Span, style::{Style, Stylize}};
+use ratatui::{
+    prelude::{Constraint, CrosstermBackend, Direction, Layout, Rect},
+    style::{Style, Stylize},
+    text::Span,
+    widgets::{Block, Borders, Paragraph},
+    Frame, Terminal,
+};
 
 mod commands;
 
-pub const FRAME_RATE_MILLIS: u64 = 16;
+pub const FRAMES_PER_SECOND: u64 = 30;
 lazy_static! {
     pub static ref FRAME_WAIT_DURATION: std::time::Duration =
-        std::time::Duration::from_millis(FRAME_RATE_MILLIS);
+        std::time::Duration::from_millis(FRAMES_PER_SECOND);
 }
 
 pub struct AppState {
@@ -36,7 +47,20 @@ fn main() -> Result<()> {
         should_quit: false,
     };
 
+    let mut last_frame_time = Instant::now();
+
     while !app_state.should_quit {
+        // Draw the TUI for the current frame
+        terminal.draw(|frame: &mut Frame| {
+            // Draw a popup window
+            let popup_area = centered_rect(frame.size(), 36, 36);
+
+            frame.render_widget(
+                Block::default().borders(Borders::all()).title("Main"),
+                popup_area,
+            )
+        })?;
+
         // Check for keypress events.
         let key_opt = poll_for_keypress();
         let command_opt: Option<AppCommand> = key_opt
@@ -51,16 +75,15 @@ fn main() -> Result<()> {
             }
         };
 
-        // Draw the TUI for the current frame
-        terminal.draw(|frame: &mut Frame| {
-            // Draw a popup window
-            let popup_area = centered_rect(frame.size(), 36, 36);
+        // Sleep until the next frame
+        let elapsed = Instant::now().duration_since(last_frame_time);
 
-            frame.render_widget(
-                Block::default().borders(Borders::all()).title("Main"),
-                popup_area
-            )
-        })?;
+        // Update the last frame time now that we have evaluated time since last frame.
+        last_frame_time = std::time::Instant::now();
+
+        if elapsed < *FRAME_WAIT_DURATION {
+            std::thread::sleep(*FRAME_WAIT_DURATION - elapsed);
+        }
     }
 
     // Broke out of loop, initiate shutdown.
@@ -92,7 +115,8 @@ fn centered_rect(r: Rect, percent_x: u16, percent_y: u16) -> Rect {
             Constraint::Percentage((100 - percent_y) / 2),
             Constraint::Percentage(percent_y),
             Constraint::Percentage((100 - percent_y) / 2),
-        ]).split(r);
+        ])
+        .split(r);
 
     Layout::default()
         .direction(Direction::Horizontal)
@@ -100,7 +124,8 @@ fn centered_rect(r: Rect, percent_x: u16, percent_y: u16) -> Rect {
             Constraint::Percentage((100 - percent_x) / 2),
             Constraint::Percentage(percent_x),
             Constraint::Percentage((100 - percent_x) / 2),
-        ]).split(popup_layout[1])[1]
+        ])
+        .split(popup_layout[1])[1]
 }
 
 fn ui_yellow_span(_app_state: &AppState, frame: &mut Frame) {
